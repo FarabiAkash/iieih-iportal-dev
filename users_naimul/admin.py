0b38830988333964f7d512dc4e072d6746ca36ee
naimulislamsaikat <nzsaikat06@gmail.com>
2026-09-22 17:22:53 +0600

    login UI created

--- users_naimul/admin.py
+++ users_naimul/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from users_naimul.models import SurgeryRecord
+from users_naimul.models import DiagnosisProcedure
 
 # Register your models here.
 admin.site.register(SurgeryRecord)
+admin.site.register(DiagnosisProcedure)
--- users_naimul/models.py
+++ users_naimul/models.py
@@ -70,32 +70,64 @@ class SurgeryRecord(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, help_text="User who created this record")
     created_at = models.DateTimeField(auto_now_add=True, help_text="Timestamp when the record was created")
     
-# class DiagnosisRecord(models.Model):
+class DiagnosisProcedure(models.Model):
     
-#     Eye_Choices = [
-#         ('LE', 'Left Eye'),
-#         ('RE', 'Right Eye'),
-#         ('BE', 'Both Eyes'),
-#     ]
+    Eye_Choices = [
+        ('LE', 'Left Eye'),
+        ('RE', 'Right Eye'),
+        ('BE', 'Both Eyes'),
+    ]
     
-#     Gender_Choices = [
-#         ('Male', 'Male'),
-#         ('Female', 'Female'),
-#         ('Other', 'Other'),
-#     ]
+    Gender_Choices = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    ]
     
-#     Procedure_Choices = [
-#         ('Cataract', 'Cataract'),
-#         ('Glaucoma', 'Glaucoma'),
-#         ('Retina', 'Retina'),
-#         ('Cornea', 'Cornea'),
-#         ('LASIK', 'LASIK'),
-#         ('Oculoplastic', 'Oculoplastic'),
-#         ('Pediatric Ophthalmology', 'Pediatric Ophthalmology'),
-#         ('Neuro-Ophthalmology', 'Neuro-Ophthalmology'),
-#         ('Uveitis', 'Uveitis'),
-#         ('Refractive Surgery', 'Refractive Surgery'),
-#         ('Other', 'Other'),
-#     ]
+    Procedure_Choices = [        
+        ('OCT Macula', 'OCT Macula'),
+        ('OCT RNFL', 'OCT RNFL'),
+        ('Visual Field (Perimetry)', 'Visual Field (Perimetry)'),
+        ('B-Scan', 'B-Scan'),
+        ('FFA', 'FFA'),
+        ('Other', 'Other'),
+    ]
+    
+    date = models.DateField(help_text="Date of Diagnosis (YYYY-MM-DD)")
+    patient_mrn = models.CharField(max_length=8, unique=True, help_text="Medical Record Number (MRN) of the Patient")
+    eye = models.CharField(max_length=2, choices=Eye_Choices, help_text="Which eye was diagnosed")
+    procedure_name = models.CharField(max_length=50, choices=Procedure_Choices, help_text="Procedure Performed")
+    technician_name = models.CharField(max_length=100, help_text="Name of the Technician")
+    findings_summary = models.TextField(blank=True, help_text="Findings summary from the Diagnosis")
+    
+class DoctorProfile(models.Model):
+    
+    Specialty_Choices = [
+        ('Cataract', 'Cataract'),
+        ('Glaucoma', 'Glaucoma'),
+        ('Retina', 'Retina'),
+        ('Cornea', 'Cornea'),
+        ('Oculoplastic', 'Oculoplastic'),
+        ('Pediatric Ophthalmology', 'Pediatric Ophthalmology'),
+        ('Neuro-Ophthalmology', 'Neuro-Ophthalmology'),
+        ('Uveitis', 'Uveitis'),
+        ('Refractive Surgery', 'Refractive Surgery'),
+        ('Other', 'Other'),
+    ]
+    
+    OT_Room_Choices = [
+        ('OT-1', 'OT Room 1 (General OT)'),
+        ('OT-2', 'OT Room 2 (Retina OT)'),
+        ('OT-3', 'OT Room 3 (Laser OT)'),
+        ('OT-4', 'OT Room 4 (Pediatric OT)'),
+    ]
+    
+    Role_Choices = [
+        ('Surgeon', 'Surgeon'),
+        ('Technician', 'Technician'),
+        ('Nurse', 'Nurse'),
+        ('Admin', 'Admin'),
+        ('Other', 'Other'),
+    ]
     
     
